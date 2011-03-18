8cc437af2b4d8b2318e8bee0a0b13075ebc62ef6
Maik Hentsche <maik.hentsche@amd.com>
2011-03-18 15:14:00 +0100

    Refactoring to make file upload implementation possible

--- lib/Tapper/TestSuite/AutoTest.pm
+++ lib/Tapper/TestSuite/AutoTest.pm
@@ -2,6 +2,7 @@ package Tapper::TestSuite::AutoTest;
 
 use warnings;
 use strict;
+use 5.010;
 
 use Moose;
 use Getopt::Long qw/GetOptions/;
@@ -10,7 +11,7 @@ use Sys::Hostname qw/hostname/;
 use YAML::Syck;
 use Archive::Tar;
 use IO::Socket::INET;
-
+use File::Slurp qw/slurp/;
 
 extends 'Tapper::Base';
 
@@ -33,7 +34,6 @@ This module wraps autotest to make its (sub) tests available for Tapper.
     my $args    = $wrapper->parse_args();
     $args       = $wrapper->install($args);
     $args       = $wrapper->run($args);
-    $args       = $wrapper->send_results($args);
 
 
 =head1 FUNCTIONS
@@ -62,6 +62,68 @@ sub install
 }
 
 
+=head2 report_away
+
+Send the actual report to reports receiver framework.
+
+@param hash ref - args
+
+@return success - int - report id
+@return error   - die()
+
+=cut
+
+sub report_away
+{
+        my ($self, $args) = @_;
+        my $result_dir   = $args->{result_dir};
+        my $gzipped_content = slurp("$result_dir/tap.tar.gz");
+
+        my $sock = IO::Socket::INET->new(PeerAddr => $args->{report_server},
+                                         PeerPort => $args->{report_port},
+                                         Proto    => 'tcp');
+        unless ($sock) { die "Can't open connection to ", $args->{report_server}, ":$!" }
+
+        my $report_id = <$sock>;
+        ($report_id) = $report_id =~ /(\d+)$/;
+        $sock->print($gzipped_content);
+        $sock->close();
+        return $report_id;
+}
+
+=head2 upload_stats
+
+Upload the stats file to reports framework.
+
+@param int       - report id
+@param hash ref - args
+
+=cut
+
+sub upload_stats
+{
+        my ($self, $report_id, $args) = @_;
+
+        my $host = $args->{reportserver};
+        my $port = $args->{reportport};
+
+        my $file    = $args->{result_dir}."/status";
+        my $cmdline = "#! upload $report_id status plain\n";
+        my $content = slurp($file);
+
+
+        my $sock = IO::Socket::INET->new(PeerAddr => $args->{report_server},
+                                         PeerPort => $args->{report_port},
+                                         Proto    => 'tcp');
+        unless ($sock) { die "Can't open connection to ", $args->{report_server}, ":$!" }
+
+        $sock->print($cmdline);
+        $sock->print($content);
+        $sock->close();
+
+        return;
+}
+
 =head2 send_results
 
 Send the test results to Tapper.
@@ -74,21 +136,28 @@ Send the test results to Tapper.
 
 sub send_results
 {
-        my ($self, $args) = @_;
+        my ($self, $test, $args) = @_;
         my $report;
 
 
-        my $tar  = Archive::Tar->new;
-        my $result_dir = $args->{target}."/results/default/";
-        my $hostname = hostname();
+        my $tar             = Archive::Tar->new;
+        $args->{result_dir} = $args->{target}."/results/default/";
+        my $result_dir      = $args->{result_dir};
+        my $hostname        = hostname();
+        my $testrun_id      = $args->{testrun_id};
+
+        my $report_group    = $args->{report_group};
+
         my $report_meta = "
 Version 13
 1..1
-# Tapper-Suite-Name: Tapper-TestSuite-AutoTest
+# Tapper-Suite-Name: Autotest-$test
 # Tapper-Machine-Name: $hostname
-# Tapper-Suite-Version: 3.000001
-ok 1 - Getting hardware information
+# Tapper-Suite-Version: $VERSION
+ok 1 - Tapper metainfo
 ";
+        $report_meta .= $testrun_id   ? "# Tapper-Reportgroup-Testrun: $testrun_id\n"     : '';
+        $report_meta .= $report_group ? "# Tapper-Reportgroup-Arbitrary: $report_group\n" : '';
 
         my $meta = YAML::Syck::LoadFile("$result_dir/meta.yml");
         push @{$meta->{file_order}}, 'tapper-suite-meta.tap';
@@ -97,23 +166,29 @@ ok 1 - Getting hardware information
         $tar->add_data('tapper-suite-meta.tap',$report_meta);
         $tar->write("$result_dir/tap.tar.gz", COMPRESS_GZIP);
 
-        my $gzipped_content;
-        {
-                open my $fh, "<", "$result_dir/tap.tar.gz";
-                local $/;
-                $gzipped_content = <$fh>;
-                close $fh;
-        }
-
-
-        my $sock = IO::Socket::INET->new(PeerAddr => $args->{report_server},
-                                         PeerPort => $args->{report_port},
-                                         Proto    => 'tcp');
-        unless ($sock) { die "Can't open connection to ", $args->{report_server}, ":$!" }
-
-        $sock->print($gzipped_content);
-        $sock->close();
+        my $report_id = $self->report_away($args);
+        $self->upload_stats($report_id);
         return $args;
+}
+
+
+=head2 print_help
+
+Print help and die.
+
+=cut
+
+sub print_help
+{
+        my ($self) = @_;
+        say "$0 --test=s@ [ --directory=s ] [--remote-name]";
+        say "\t--test|t\t\tName of a subtest, REQUIRED, may be given multple times";
+        say "\t--directory|d\t\tDirectory to copy autotest to";
+        say "\t--remote-name|O\t\tPrint out the name of result files";
+        say "\t--help|h\t\tPrint this help text and exit";
+
+
+        exit;
 }
 
 =head2 parse_args
@@ -128,18 +203,27 @@ sub parse_args
 {
         my ($self) = @_;
         my @tests;
-        my $dir;
+        my ($dir, $remote_name, $help);
+
         GetOptions ("test|t=s"  => \@tests,
                     "directory|d=s" => \$dir,
+                    "remote-name|O" => \$remote_name,
+                    "help|h"        => \$help,
                    );
-
-
+        $self->print_help() if $help;
+        if (not @tests) {
+                print "No subtest requested provided. Please name at least one subtest you want to run\n\n.";
+                $self->print_help();
+        }
 
         my $args = {subtests        => \@tests,
                     target          => $dir,
                     report_server   => $ENV{TAPPER_REPORT_SERVER}   || 'tapper',
                     report_api_port => $ENV{TAPPER_REPORT_API_PORT} || 7358,
                     report_port     => $ENV{TAPPER_REPORT_PORT}     || 7357,
+                    testrun_id      => $ENV{TAPPER_TESTRUN}         || '',
+                    report_group    => $ENV{TAPPER_REPORT_GROUP}    || '',
+                    remote_name     => $remote_name,
                    };
 
         return $args;
@@ -167,6 +251,7 @@ sub run
         foreach my $test (@{$args->{subtests} || [] }) {
                 my $test_path = "$target/tests/$test/control";
                 $self->log_and_exec($autotest, "--tap", $test_path);
+                $self->send_results($test, $args);
         }
         return $args;
 }
