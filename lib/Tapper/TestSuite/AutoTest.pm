612fc2c95b535da4457d6f4acb6c3a3686ceda1f
Steffen Schwigon <steffen.schwigon@amd.com>
2012-10-04 17:14:16 +0200

    become autonomous from Tapper
    We no longer derive from Tapper::Base. This
    produces some minor code duplication but
    makes the autotest wrapper more versatile.

--- lib/Tapper/TestSuite/AutoTest.pm
+++ lib/Tapper/TestSuite/AutoTest.pm
@@ -15,7 +15,7 @@ use File::Slurp qw/slurp/;
 use File::Spec::Functions 'tmpdir';
 use Digest::MD5 'md5_hex';
 
-extends 'Tapper::Base';
+with 'MooseX::Log::Log4perl';
 
 =head1 SYNOPSIS
 
@@ -63,6 +63,66 @@ C<TAPPER_REPORT_SERVER> pointing to your central Tapper server.
 See the Tapper manual for more details.
 
 =head1 FUNCTIONS
+
+=head2 makedir
+
+Checks whether a given directory exists and creates it if not.
+
+@param string - directory to create
+
+@return success - 0
+@return error   - error string
+
+=cut
+
+sub makedir
+{
+        my ($self, $dir) = @_;
+        return 0 if -d $dir;
+        if (-e $dir and not -d $dir) {
+                unlink $dir;
+        }
+        system("mkdir","-p",$dir) == 0 or return "Can't create $dir:$!";
+        return 0;
+}
+
+
+=head2 log_and_exec
+
+Execute a given command. Make sure the command is logged if requested and none
+of its output pollutes the console. In scalar context the function returns 0
+for success and the output of the command on error. In array context the
+function always return a list containing the return value of the command and
+the output of the command.
+
+@param string - command
+
+@return success - 0
+@return error   - error string
+@returnlist success - (0, output)
+@returnlist error   - (return value of command, output)
+
+=cut
+
+sub log_and_exec
+{
+        my ($self, @cmd) = @_;
+        my $cmd = join " ",@cmd;
+        $self->log->debug( $cmd );
+        my $output=`$cmd 2>&1`;
+        my $retval=$?;
+        if (not defined($output)) {
+                $output = "Executing $cmd failed";
+                $retval = 1;
+        }
+        chomp $output if $output;
+        if ($retval) {
+                return ($retval >> 8, $output) if wantarray;
+                return $output;
+        }
+        return (0, $output) if wantarray;
+        return 0;
+}
 
 =head2 copy_client
 
